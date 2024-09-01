db61574ada5baeae400a192a3af02ce0d38e5e7e
Gerald Planty <plantygerald02@gmail.com>
2024-09-01 11:07:35 -0400

    Fonts setup and Main/WeatherCard Started

---
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,13 @@
+import "./WeatherCard.css";
+import sunny from "../../images/sunny.svg";
+
+function WeatherCard() {
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp">75&deg; F</p>
+      <img src={sunny} alt="" className="weather-card__image" />
+    </section>
+  );
+}
+
+export default WeatherCard;
