f45c68a304ceed2a9e581b4a01646b3361a3f082
Gerald Planty <plantygerald02@gmail.com>
2024-09-06 14:21:06 -0400

    Basic Cards COmpleted

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
+
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "cold" });
+
   return (
     <div className="app">
       <div className="app_content">
         <Header />
-        <Main></Main>
+        <Main weatherData={weatherData} />
       </div>
     </div>
   );
