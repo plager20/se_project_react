9be86138274273a4074e05cec7a43857daced285
Gerald Planty <plantygerald02@gmail.com>
2024-09-12 18:57:00 -0400

    WeatherCard changes with weather

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,11 +1,30 @@
 import "./WeatherCard.css";
-import sunny from "../../images/sunny.svg";
+import { weatherOptions } from "../../utils/constants";
+import { defaultWeatherOptions } from "../../utils/constants";
 
 function WeatherCard({ weatherData }) {
+  const filteredOptions = weatherOptions.filter((option) => {
+    return (
+      option.day === weatherData.isDay &&
+      option.condition === weatherData.condition
+    );
+  });
+
+  let weatherOption;
+  if (filteredOptions.length === 0) {
+    weatherOption = defaultWeatherOptions[weatherData.isDay ? "day" : "night"];
+  } else {
+    weatherOption = filteredOptions[0];
+  }
+
   return (
     <section className="weather-card">
       <p className="weather-card__temp">{weatherData.temp.F}&deg; F</p>
-      <img src={sunny} alt="" className="weather-card__image" />
+      <img
+        src={weatherOption?.url}
+        alt={weatherOption?.condition}
+        className="weather-card__image"
+      />
     </section>
   );
 }
